440a312b9ca6e4e2bdd712991753b150ca25f06b
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-10-18 08:25:28 +0700

    feat: add @radix-ui/react-select and nanoid dependencies; update useJournalForm to generate unique journal entries

--- src/hooks/useJournalForm.ts
+++ src/hooks/useJournalForm.ts
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import type { UseFormReturn } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import { nanoid } from 'nanoid/non-secure'
 
 const formSchema = z.object({
   mistakes: z.string().trim().nonempty('Mistakes are required'),
@@ -12,6 +13,13 @@ const formSchema = z.object({
 })
 
 export type FormValues = z.infer<typeof formSchema>
+
+type JournalData = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  data: FormValues
+}
 
 export function useJournalForm(externalForm?: UseFormReturn<FormValues>) {
   const form = useForm<FormValues>({
@@ -26,7 +34,13 @@ export function useJournalForm(externalForm?: UseFormReturn<FormValues>) {
   })
 
   const onSubmit = (data: FormValues) => {
-    console.log(data)
+    const journalData: JournalData = {
+      id: nanoid(),
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      data: data,
+    }
+    console.log(journalData)
   }
 
   const handleSubmit = (externalForm || form).handleSubmit(onSubmit)
